d9ac4b858df59463871ebc23f4bef60e26c6d15f
Dimitris Raviolos <dictummortuum339@gmail.com>
2019-12-10 18:10:28 +0200

    serving static files from bindata

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/DictumMortuum/servus
 go 1.13
 
 require (
+	github.com/elazarl/go-bindata-assetfs v1.0.0
 	github.com/gin-gonic/gin v1.5.0
+	github.com/go-bindata/go-bindata v3.1.2+incompatible // indirect
 	github.com/jessevdk/go-assets v0.0.0-20160921144138-4f4301a06e15
+	github.com/jteeuwen/go-bindata v3.0.7+incompatible // indirect
 )
--- main.go
+++ main.go
@@ -1,47 +1,18 @@
 package main
 
 import (
-	"html/template"
-	"io/ioutil"
-
 	"github.com/gin-gonic/gin"
 )
 
-func loadTemplate() (*template.Template, error) {
-	t := template.New("")
-
-	for name, file := range Assets.Files {
-		h, err := ioutil.ReadAll(file)
-
-		if err != nil {
-			return nil, err
-		}
-
-		t, err = t.New(name).Parse(string(h))
-
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	return t, nil
+func startpageHandler(c *gin.Context) {
+	data, _ := Asset("html/index.html")
+	c.Writer.Write(data)
 }
 
 func main() {
+	files := assetFS()
 	r := gin.Default()
-
-	t, err := loadTemplate()
-
-	if err != nil {
-		panic(err)
-	}
-
-	r.SetHTMLTemplate(t)
-	r.Static("/assets", "./assets")
-
-	r.GET("/startpage", func(c *gin.Context) {
-		c.HTML(200, "/html/index.html", nil)
-	})
-
+	r.StaticFS("/assets", files)
+	r.GET("/startpage", startpageHandler)
 	r.Run(":1234")
 }
