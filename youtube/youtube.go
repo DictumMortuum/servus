f767cf0eead9770fd87aac394efae22b8964dfdc
DictumMortuum <theio.vrefos@gmail.com>
2020-08-17 21:38:56 +0300

    removing youtube check

--- youtube/youtube.go
+++ youtube/youtube.go
@@ -4,17 +4,10 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"os"
-	"regexp"
 )
 
 func Handler(c *gin.Context) {
 	url := c.Param("url")
-	re := regexp.MustCompile(`youtube`)
-
-	if !re.MatchString(url) {
-		c.JSON(http.StatusOK, gin.H{"status": "not a youtube link"})
-		return
-	}
 
 	f, err := os.OpenFile("/var/lib/servus/youtube.list", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
