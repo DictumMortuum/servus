9f4f961dd43e8f7cfb716aa9bfbf0b55c442ee3a
DictumMortuum <theio.vrefos@gmail.com>
2021-09-12 14:29:30 +0300

    [pkg/boardgames] added Quacks of Quedlingburg scoring

--- pkg/boardgames/score.go
+++ pkg/boardgames/score.go
@@ -152,6 +152,8 @@ func getFuncs(boardgame_id int64) (func(models.Stats) float64, func([]models.Sta
 		return CaledoniaScore, DefaultSort
 	case 276025:
 		return DefaultScore, DefaultSort
+	case 244521:
+		return DefaultScore, DefaultSort
 	default:
 		return nil, nil
 	}
