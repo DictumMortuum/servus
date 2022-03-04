7d8cf1970cecc301f2ee1c55891573e98cd36a69
DictumMortuum <theio.vrefos@gmail.com>
2022-03-04 10:56:29 +0200

    [pkg/boardgames/mapping] added MapAllBgg that automatically maps a price to the bgg id, if the API only returns one result

--- pkg/boardgames/mapping/bgg.go
+++ pkg/boardgames/mapping/bgg.go
@@ -1,11 +1,13 @@
 package mapping
 
 import (
+	"fmt"
 	"github.com/DictumMortuum/servus/pkg/boardgames"
 	"github.com/DictumMortuum/servus/pkg/boardgames/bgg"
 	"github.com/DictumMortuum/servus/pkg/models"
 	"github.com/jmoiron/sqlx"
 	"log"
+	"time"
 )
 
 func MapBGG(db *sqlx.DB, args *models.QueryBuilder) (interface{}, error) {
@@ -53,4 +55,45 @@ func SearchBGGTerm(db *sqlx.DB, args *models.QueryBuilder) (interface{}, error) 
 	}
 
 	return rs, nil
+}
+
+func MapAllBgg(db *sqlx.DB, args *models.QueryBuilder) (interface{}, error) {
+	prices, err := getPricesWithoutMappings(db)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, price := range prices {
+		updatePriceNotMapped(db, price)
+	}
+
+	retval := []models.Price{}
+
+	l := len(prices)
+
+	for i, price := range prices {
+		name := transformName(price.Name)
+
+		bgg_results, err := bgg.Search(name)
+		if err != nil {
+			return nil, err
+		}
+
+		if len(bgg_results) == 1 {
+			price.BoardgameId = models.JsonNullInt64{
+				Int64: bgg_results[0].Id,
+				Valid: true,
+			}
+
+			fmt.Printf("[%5v/%v] %v to %v\n", i, l, price.Name, bgg_results[0].Id)
+
+			updatePrice(db, price)
+
+			retval = append(retval, price)
+		}
+
+		time.Sleep(3 * time.Second)
+	}
+
+	return retval, nil
 }
