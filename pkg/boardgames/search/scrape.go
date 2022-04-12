d8f2e1dde84ee3a436e5f3271083623493974ac5
DictumMortuum <theio.vrefos@gmail.com>
2022-04-12 19:53:02 +0300

    [pkg/boardgames/search] added xrysoftero

--- pkg/boardgames/search/scrape.go
+++ pkg/boardgames/search/scrape.go
@@ -27,6 +27,7 @@ var allowed_domains = []string{
 	"epitrapez.io",
 	"www.ozon.gr",
 	"gamesuniverse.gr",
+	"xrysoftero.gr",
 }
 
 func initializeScraper(pwd string) (*colly.Collector, *queue.Queue, *store.Storage, error) {
@@ -448,6 +449,37 @@ func Scrape(db *sqlx.DB, batch_id *models.JsonNullInt64) ([]models.Price, error)
 		})
 	})
 
+	collector.OnHTML("a.next", func(e *colly.HTMLElement) {
+		if !strings.Contains(e.Request.URL.String(), "xrysoftero.gr") {
+			return
+		}
+
+		link := e.Attr("href")
+		log.Println("Visiting: " + link)
+		queue.AddURL(link)
+	})
+
+	collector.OnHTML(".thumbnail-container", func(e *colly.HTMLElement) {
+		if !strings.Contains(e.Request.URL.String(), "xrysoftero.gr") {
+			return
+		}
+
+		url := e.ChildAttr(".cover-image", "src")
+		if url == "" {
+			return
+		}
+
+		raw_price := e.ChildText(".price")
+		rs = append(rs, models.Price{
+			Name:       e.ChildText(".product-title"),
+			StoreId:    21,
+			StoreThumb: url,
+			Stock:      true,
+			Price:      getPrice(raw_price),
+			Url:        e.ChildAttr("a.relative", "href"),
+		})
+	})
+
 	// // No Label X
 	// collector.OnHTML("a.next", func(e *colly.HTMLElement) {
 	// 	if !strings.Contains(e.Request.URL.String(), "www.skroutz.gr") {
@@ -491,6 +523,7 @@ func Scrape(db *sqlx.DB, batch_id *models.JsonNullInt64) ([]models.Price, error)
 	queue.AddURL("https://www.ozon.gr/pazl-kai-paixnidia/epitrapezia-paixnidia")
 	queue.AddURL("https://gamesuniverse.gr/el/10-epitrapezia")
 	queue.AddURL("https://www.politeianet.gr/index.php?option=com_virtuemart&category_id=948&page=shop.browse&subCatFilter=-1&langFilter=-1&pubdateFilter=-1&availabilityFilter=-1&discountFilter=-1&priceFilter=-1&pageFilter=-1&Itemid=721&limit=20&limitstart=0")
+	queue.AddURL("https://xrysoftero.gr/362-epitrapezia-paixnidia?resultsPerPage=48&q=%CE%9C%CE%AC%CF%81%CE%BA%CE%B1%5C-%CE%95%CE%BA%CE%B4%CF%8C%CF%84%CE%B7%CF%82-%CE%9A%CE%AC%CE%B9%CF%83%CF%83%CE%B1")
 	queue.Run(collector)
 
 	err = storage.Clear()
