002c624a54cb2191f01c0cd88d520da5be42cb96
DictumMortuum <theio.vrefos@gmail.com>
2022-09-19 23:41:35 +0300

    [pkg/models] added modem

---
+++ pkg/models/modem.go
@@ -0,0 +1,19 @@
+package models
+
+type Modem struct {
+	Uptime      int64   `json:"uptime"`
+	Status      bool    `json:"status"`
+	VoipStatus  bool    `json:"voip_status"`
+	CurrentUp   int     `json:"current_up"`
+	CurrentDown int     `json:"current_down"`
+	MaxUp       int     `json:"max_up"`
+	MaxDown     int     `json:"max_down"`
+	DataUp      int64   `json:"data_up"`
+	DataDown    int64   `json:"data_down"`
+	FECUp       int     `json:"fec_up"`
+	FECDown     int     `json:"fec_down"`
+	CRCUp       int     `json:"crc_up"`
+	CRCDown     int     `json:"crc_down"`
+	SNRUp       float64 `json:"snr_up"`
+	SNRDown     float64 `json:"snr_down"`
+}
